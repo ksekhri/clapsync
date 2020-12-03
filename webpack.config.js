e72fc0f2a9cf93042d811d8e8f473f73e3d461e1
Karan Sekhri <github@ksekhri.com>
2020-12-02 21:48:13 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|jsx|tsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images'
+            },
+          },
+        ],
+      },
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx', '.ts', '.tsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    filename: 'js/bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+    publicPath: '/',
+    hotOnly: true,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      template: path.resolve(__dirname, 'src/index.ejs'),
+      scriptLoading: 'defer',
+      title: 'Page Title',
+      appMountId: 'root',
+      noscriptText: 'Requires JavaScript',
+      meta: [
+        {
+          name: 'description',
+          content: 'Search engine description',
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width',
+        },
+        {
+          charset: 'utf-8'
+        },
+      ],
+    })
+  ],
+}
